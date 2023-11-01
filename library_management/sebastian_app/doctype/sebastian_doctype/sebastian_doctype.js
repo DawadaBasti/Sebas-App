ff26214b35fa60a0032d9b829c0d01eff68e343f
DawadaBasti <1823010111@untels.edu.pe>
2023-11-01 12:05:17 -0500

    create workspace and update side bar

---
+++ library_management/sebastian_app/doctype/sebastian_doctype/sebastian_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Bahubali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sebastian Doctype', {
+	refresh: function(frm) {
+        console.log('hola')
+	}
+});
---
+++ library_management/sebastian_app/doctype/sebastian_doctype/test_sebastian_doctype.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Bahubali and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestSebastianDoctype(unittest.TestCase):
+	pass
